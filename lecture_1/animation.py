a2d9abcda33488fcded6cc98b81ee5f14c68c839
etorch <dannycharlessh@gmail.com>
2024-02-22 23:09:35 +0300

    add first task

--- lecture_1/animation.py
+++ lecture_1/animation.py
@@ -10,54 +10,47 @@ k = 20
 m = 1
 b = 0.25
 
-# position=-1, velocity=0
-x0 = -1
-x_dot0 = 0
+# Начальные условия для двух точек и их скоростей (x1, x2, x1_dot, x2_dot)
+x1_0 = -2
+x2_0 = 2
+x1_dot0 = 0
+x2_dot0 = 0
 
-# y[0]=x, y[1]=x_dot
-def spring_mass_ODE(t, y): 
-    return (y[1], g - k*y[0]/m - b*y[1]/m)
+def two_spring_masses_ODE(t, y):
+    return [
+        y[1],  # x1_dot
+        (k/m) * (y[2] - y[0]) - b * y[1],
+        y[3],  # x2_dot
+        -k/m * (y[2] - y[0]) - b * y[3]
+    ]
 
-sol = solve_ivp(spring_mass_ODE, [0, 10], (x0, x_dot0), 
-    t_eval=np.linspace(0,10,10*30))
 
-x, x_dot = sol.y
-t = sol.t
+sol = solve_ivp(two_spring_masses_ODE, [0, 10], [x1_0, x1_dot0, x2_0, x2_dot0], 
+                t_eval=np.linspace(0, 10, 10*30))
+
+positions = sol.y
+times = sol.t
 
 fig = plt.figure()
 ax = fig.add_subplot(aspect='equal')
-ax.set_xlim(-1, 1)
-ax.set_ylim(-5, 0)
+ax.set_xlim(-3, 3)
+ax.set_ylim(-1, 1)
 
-def generate_spring(n):
-    data = np.zeros((2,n+2))
-    for i in range(1,n+1):
-        data[0,i] = -1/(2*n) if i % 2 else 1/(2*n)
-        data[1,i] = -(2*i-1)/(2*n)
-    return data
-
-data = np.append(generate_spring(30), np.ones((1,30+2)), axis=0)
-
-ell = 2          
-y0 = -(ell + x0)
-spring = Line2D(data[0,:], data[1,:], color='g')
-circle = ax.add_patch(plt.Circle( (0,y0), 0.25, fc='orange', ec='none', zorder=3))
+spring = Line2D([], [], color='g')
 ax.add_line(spring)
 
+circle1 = ax.add_patch(plt.Circle((positions[0, 0], 0), 0.2, fc='orange', ec='none', zorder=3))
+circle2 = ax.add_patch(plt.Circle((positions[2, 0], 0), 0.2, fc='blue', ec='none', zorder=3))
+
 def animate(i):
-    y = -(ell + x[i])
-    circle.set_center((0, y))
-
-    stretch_factor = -y
+    x1, x1_dot, x2, x2_dot = positions[:, i]
     
-    A = Affine2D().scale(8/stretch_factor, stretch_factor).get_matrix()
-    data_new = np.matmul(A, data)
+    data = np.array([[x1, x2], [0, 0]])
+    spring.set_data(data[0, :], data[1, :])
 
-    data_new[0, :] += np.random.uniform(-0.02, 0.02, size=data_new.shape[1])
-    data_new[1, :] += np.random.uniform(-0.02, 0.02, size=data_new.shape[1])
+    circle1.set_center((x1, 0))
+    circle2.set_center((x2, 0))
 
-    spring.set_data(data_new[0, :], data_new[1, :])
-
-ani = animation.FuncAnimation(fig, animate, frames=len(t))
+ani = animation.FuncAnimation(fig, animate, frames=len(times))
 ffmpeg_writer = animation.FFMpegWriter(fps=30)
-ani.save('simulation_in_1D.gif', writer=ffmpeg_writer)
+ani.save('simulation_in_1D_with_springs.gif', writer=ffmpeg_writer)
