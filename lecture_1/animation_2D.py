639d76e028d01cb3f983bd506c5268e99c7d7b52
etorch <dannycharlessh@gmail.com>
2024-02-24 19:21:52 +0300

    fixed 2nd task

--- lecture_1/animation_2D.py
+++ lecture_1/animation_2D.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from scipy.integrate import solve_ivp
 import matplotlib.animation as animation
-from matplotlib.transforms import Affine2D
-
 
 g = 9.81
 k = 20
@@ -12,9 +10,9 @@ m = 1
 b = 0.25
 
 x1_0 = -2
-y1_0 = 0
+y1_0 = -2
 x2_0 = 2
-y2_0 = 0
+y2_0 = 2
 x1_dot0 = 0
 y1_dot0 = 0
 x2_dot0 = 0
@@ -22,23 +20,21 @@ y2_dot0 = 0
 
 def two_spring_masses_ODE(t, y):
     x1, y1, x1_dot, y1_dot, x2, y2, x2_dot, y2_dot = y
-    return [
-        x1_dot, 
-        y1_dot,  
-        (k/m) * (x2 - x1) - b * x1_dot,  
-        (k/m) * (y2 - y1) - b * y1_dot, 
-        x2_dot,  
-        y2_dot,  
-        -k/m * (x2 - x1) - b * x2_dot, 
-        -k/m * (y2 - y1) - b * y2_dot 
-    ]
+    dx1_dt = x1_dot
+    dy1_dt = y1_dot
+    dx1_dot_dt = (k/m) * (x2 - x1) - b * x1_dot
+    dy1_dot_dt = (k/m) * (y2 - y1) - b * y1_dot
+    dx2_dt = x2_dot
+    dy2_dt = y2_dot
+    dx2_dot_dt = -k/m * (x2 - x1) - b * x2_dot
+    dy2_dot_dt = -k/m * (y2 - y1) - b * y2_dot
+    return [dx1_dt, dy1_dt, dx1_dot_dt, dy1_dot_dt, dx2_dt, dy2_dt, dx2_dot_dt, dy2_dot_dt]
 
 sol = solve_ivp(two_spring_masses_ODE, [0, 10], [x1_0, y1_0, x1_dot0, y1_dot0, x2_0, y2_0, x2_dot0, y2_dot0], 
                 t_eval=np.linspace(0, 10, 10*30))
 
 positions = sol.y
 times = sol.t
-
 
 fig = plt.figure()
 ax = fig.add_subplot(aspect='equal')
@@ -62,4 +58,6 @@ def animate(i):
 
 ani = animation.FuncAnimation(fig, animate, frames=len(times))
 ffmpeg_writer = animation.FFMpegWriter(fps=30)
-ani.save('simulation_in_2D.gif', writer=ffmpeg_writer)
+ani.save('simulation_in_2D_diagonal.gif', writer=ffmpeg_writer)
+
+plt.show()
